adc4b58869db8f4e4826066538c6fed4351d91a0
simon coulombe <simon.coulombe@cooperators.ca>
2025-01-31 13:20:18 -0500

    fixed namespace rlang %||%

--- R/shinydbanalysis-package.R
+++ R/shinydbanalysis-package.R
@@ -6,7 +6,6 @@
 #' @importFrom dplyr group_by summarise filter tbl collect mutate  select collect n  arrange pull distinct
 #' @importFrom tidyr drop_na
 #' @importFrom dbplyr sql
-#' @importFrom rlang parse_expr sym syms
-#' @importFrom DT renderDataTable  dataTableOutput
+#' @importFrom rlang parse_expr sym syms %||%
 #' @import shiny
 NULL
